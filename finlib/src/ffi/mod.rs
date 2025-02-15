37f4d8f84c28dab267c4557bf49cc77901efd3f2
Andy Pack <andy@sarsoo.xyz>
2025-02-15 20:27:44 +0000

    rearranging ffi mods, adding wasm and python builds

---
+++ finlib/src/ffi/mod.rs
@@ -0,0 +1,4 @@
+#[cfg(feature = "py")]
+pub mod py;
+#[cfg(feature = "wasm")]
+pub mod wasm;
--- finlib/src/lib.rs
+++ finlib/src/lib.rs
@@ -2,7 +2,4 @@ pub mod interest;
 pub mod stats;
 pub mod util;
 pub mod risk;
-#[cfg(feature = "py")]
-pub mod py;
-#[cfg(feature = "wasm")]
-pub mod wasm;
+pub mod ffi;
